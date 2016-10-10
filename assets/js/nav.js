c2ba2c95cd1d26f9efe87135f1331598cb9f8269
Erbnlegend <erbnlegend@gmail.com>
2016-10-10 01:35:57 -0700

    Minor Adjustments to style

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,4 +5,19 @@ $(document).ready(function() {
   $('#nav p').click(function(event) {
     $('#nav').animate({opacity: 'toggle'}, 200);
   });
+
+  // Smooth Scroll Effect
+
+  	$('a[href^="#"]').on('click',function (e) {
+  	    e.preventDefault();
+
+  	    var target = this.hash;
+  	    var $target = $(target);
+
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 1000, 'swing', function () {
+  	        window.location.hash = target;
+  	    });
+  	});
 });
